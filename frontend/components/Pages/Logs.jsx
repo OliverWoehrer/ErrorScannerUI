2552ddf9cd90b20700f3f23ac9ebc52a3bd71db7
OliverWoehrer <oliver.woehrer@gmail.com>
2025-11-07 19:06:25 +0100

    re-implementation with shared filter element

--- frontend/components/Pages/Logs.jsx
+++ frontend/components/Pages/Logs.jsx
@@ -11,99 +11,57 @@ import 'mdui/components/button.js';
 import 'mdui/components/button-icon.js';
 import 'mdui/components/card.js';
 import 'mdui/components/chip.js';
+import 'mdui/components/collapse.js';
+import 'mdui/components/collapse-item.js';
 import 'mdui/components/list.js';
 import 'mdui/components/range-slider.js';
 import 'mdui/components/text-field.js';
 import 'mdui/components/tooltip.js';
 
 // Local Imports:
-import { openDialog, closeDialog, searchFunction, toDateString, isInsideInterval } from '../../assets/scripts.js'
-import HorizontalRow from '../HorizontalRow.jsx';
-import { useFilteredData } from '../../assets/useFilteredData.js';
-
-///////////////////////////////////////////////////////////////////////////////////////////////
-// Internal Components:
-///////////////////////////////////////////////////////////////////////////////////////////////
+import { openDialog, closeDialog } from '../../assets/scripts.js'
+import { useFetchData } from '../../assets/useFetchData.js';
+import ItemFilters from '../ItemFilters.jsx';
 
 
 function Logs() {
     ///////////////////////////////////////////////////////////////////////////////////////////////
     // Global Properties
     ///////////////////////////////////////////////////////////////////////////////////////////////
-    const {isLoading,filters,items,filteredItems,updateFilter,reloadItems} = useFilteredData("/api/logs");
+    const {isLoading,items,reloadItems} = useFetchData("/api/logs");
+    const [filteredItems, setFilteredItems] = useState(items);
     const snackBarRef = useRef(null);
     const [selectedItem, setSelectedItem] = useState(null);
 
-    
     ///////////////////////////////////////////////////////////////////////////////////////////////
     // Event Handler (triggered on user interactions):
     ///////////////////////////////////////////////////////////////////////////////////////////////
-    function openDetails(id) {
+    function showDetails(id) {
         setSelectedItem(items.find(item => item.id === id));
     };
 
-    function closeDetails() {
+    function hideDetails() {
         setSelectedItem(null);
     };
-
-    const textSearchRef = useRef(null);
-    function updateSearchQuery() {
-        const query = textSearchRef.current.value;
-        updateFilter("searchQuery", String(query));
-    }
-
-    function updateCategory(category) {
-        let selectedCategories = [];
-        if(filters.categories.includes(category)) {
-            selectedCategories = filters.categories.filter(c => c !== category); // remove category
-        } else {
-            selectedCategories = [...filters.categories, category]; // add category
-        }
-        updateFilter("categories", selectedCategories);
-    };
-
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
     // Hooks:
     ///////////////////////////////////////////////////////////////////////////////////////////////
-    const categoryRef = useRef(null);
+    // Initialize:
     useEffect(() => {
-        // Initialize Search Function:
-        if(textSearchRef.current) {
-            textSearchRef.current.addEventListener("input", updateSearchQuery);
-        }
-
-        // Initialize Search Query Filter:
-        updateFilter("searchQuery", "");
-
-        // Initialize Date Picker:
-        // TODO
-        
-        // Initialze Category Filter:
-        // const NodesList = categoryRef.current.querySelectorAll("mdui-chip[variant='filter']");
-        // const chips = Array.from(NodesList);
-        const chips = Array.from(document.querySelectorAll("mdui-chip[variant='filter']"));
-        const selectedChips = chips.filter(chip => chip.hasAttribute('selected'));
-        const categories = chips.map(chip => chip.textContent.trim());
-        const selectedCategories = selectedChips.map(chip => chip.textContent.trim());
-        updateFilter("categories", selectedCategories);
-
-        // Initialize Datetime Filter:
-        updateFilter("startDatetime", new Date());
-        updateFilter("endDatetime", new Date());
-
         // Initialze ESC Key:
         document.body.addEventListener("keydown", function(event) {
             if (event.key === "Escape") {
-                closeDetails();
+                hideDetails();
             }
         });
     }, []);
 
+    // Update Loading Animation:
     const loadingAnimationRef = useRef(null);
     useEffect(() => {
         loadingAnimationRef.current.loading = isLoading;
-    }, [isLoading]);    
+    }, [isLoading]);
 
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
@@ -111,66 +69,17 @@ function Logs() {
     ///////////////////////////////////////////////////////////////////////////////////////////////
     const ListHeader = (
         <>
-            <HorizontalRow>
-                <mdui-tooltip content="Refresh logs">
-                    <mdui-fab ref={loadingAnimationRef} onClick={() => {reloadItems()}} icon="refresh">Refresh logs</mdui-fab>
-                </mdui-tooltip>
-                <mdui-text-field ref={textSearchRef} type="search" label="Search for logs or container names" clearable helper-on-focus>
-                    <mdui-icon slot="icon" name="search"></mdui-icon>
-                </mdui-text-field>
-            </HorizontalRow>
-            <HorizontalRow>
-                <mdui-card variant="filled" style={{width:"100%"}}>
-                    <mdui-text-field label="Starting Date" helper-on-focus readonly>
-                        <mdui-icon slot="icon" name="calendar_month"></mdui-icon>
-                    </mdui-text-field>
-                    <mdui-dialog>
-                        <date-picker>
-                            <span slot="supporting-text">Select a date</span>
-                            <mdui-top-app-bar-title slot="headline"></mdui-top-app-bar-title>
-                            <mdui-button-icon slot="prev-month-btn" icon="keyboard_arrow_left"></mdui-button-icon>
-                            <mdui-button-icon slot="next-month-btn" icon="keyboard_arrow_right"></mdui-button-icon>
-                            <mdui-button-icon slot="prev-year-btn" icon="keyboard_arrow_left"></mdui-button-icon>
-                            <mdui-button-icon slot="next-year-btn" icon="keyboard_arrow_right"></mdui-button-icon>
-                            <mdui-button slot="cancel-btn" variant="text">Cancel</mdui-button>
-                            <mdui-button slot="confirm-btn">OK</mdui-button>
-                        </date-picker>
-                    </mdui-dialog>
-                    <mdui-text-field type="time" label="Starting Time" helper-on-focus>
-                        <mdui-icon slot="icon" name="access_time"></mdui-icon>
-                    </mdui-text-field>
-                </mdui-card>
-                <mdui-card variant="filled" style={{width:"100%"}}>
-                     <mdui-text-field label="Ending Date" helper-on-focus readonly>
-                        <mdui-icon slot="icon" name="calendar_month"></mdui-icon>
-                    </mdui-text-field>
-                    <mdui-dialog>
-                        <date-picker>
-                            <span slot="supporting-text">Select a date</span>
-                            <mdui-top-app-bar-title slot="headline"></mdui-top-app-bar-title>
-                            <mdui-button-icon slot="prev-month-btn" icon="keyboard_arrow_left"></mdui-button-icon>
-                            <mdui-button-icon slot="next-month-btn" icon="keyboard_arrow_right"></mdui-button-icon>
-                            <mdui-button-icon slot="prev-year-btn" icon="keyboard_arrow_left"></mdui-button-icon>
-                            <mdui-button-icon slot="next-year-btn" icon="keyboard_arrow_right"></mdui-button-icon>
-                            <mdui-button slot="cancel-btn" variant="text">Cancel</mdui-button>
-                            <mdui-button slot="confirm-btn">OK</mdui-button>
-                        </date-picker>
-                    </mdui-dialog>
-                    <mdui-text-field type="time" label="Ending Time" helper-on-focus readonly>
-                        <mdui-icon slot="icon" name="access_time"></mdui-icon>
-                    </mdui-text-field>
-                </mdui-card>
-            </HorizontalRow>
-            <HorizontalRow overflow>
-                <mdui-chip variant="filter" onClick={() => updateCategory("Critical")} selectable selected>Critical</mdui-chip>
-                <mdui-chip variant="filter" onClick={() => updateCategory("Error")} selectable selected>Error</mdui-chip>
-                <mdui-chip variant="filter" onClick={() => updateCategory("Warning")} selectable selected>Warning</mdui-chip>
-                <mdui-chip variant="filter" onClick={() => updateCategory("Info")} selectable>Info</mdui-chip>
-                <mdui-chip variant="filter" onClick={() => updateCategory("Debug")} selectable>Debug</mdui-chip>
-            </HorizontalRow>
-            <section style={{display:"flex", gap:"8px", overflowX:"auto", justifyContent:"center"}}>
-                
-            </section>
+            <mdui-collapse>
+                <mdui-collapse-item trigger="#showFilters">
+                    <div slot="header" style={{alignContent:"flex-end", alignItems:"center", display:"flex", gap:"12px", justifyContent:"space-between", padding:"12px 0.5rem 0.5rem"}}>
+                        <mdui-fab extended ref={loadingAnimationRef} onClick={() => {reloadItems()}} icon="refresh">Refresh logs</mdui-fab>
+                        <mdui-button id="showFilters" variant="text" end-icon="keyboard_arrow_down">Use filters</mdui-button>
+                    </div>
+                    <div style={{padding:"0 0.5rem"}}>
+                        <ItemFilters items={items} updateFilteredItems={setFilteredItems} />
+                    </div>
+                </mdui-collapse-item>
+            </mdui-collapse>
             <span className="info-text">Showing {filteredItems.length} of {items.length}</span>
         </>
     );
@@ -178,7 +87,7 @@ function Logs() {
     const ListPane = filteredItems.length > 0 ? (
         <>
             <mdui-list>
-                {filteredItems.map(item => (<LogItemView key={item.id} log={item} onClick={openDetails} isSelected={item === selectedItem} />))}
+                {filteredItems.map(item => (<LogItemView key={item.id} log={item} onClick={showDetails} isSelected={item === selectedItem} />))}
             </mdui-list>
         </>
     ) : (
@@ -188,9 +97,9 @@ function Logs() {
         </div>
     );
 
-    const TopBarElement = selectedItem && (
+    const DetailsTopBarElement = selectedItem && (
         <>
-            <TopBar title={"#"+selectedItem.id} closeFunction={closeDetails}>
+            <TopBar title={"#"+selectedItem.id} closeFunction={hideDetails}>
                 <mdui-button onclick={() => (openDialog("add-to-records"))}>
                     <mdui-icon slot="icon" name="add"></mdui-icon>
                     Add to records
@@ -204,7 +113,7 @@ function Logs() {
     );
 
     const DetailPane = selectedItem && (
-        <DetailsView top={TopBarElement} log={selectedItem} />
+        <DetailsView top={DetailsTopBarElement} log={selectedItem} />
     );
 
     return(
--- frontend/components/Pages/Records.jsx
+++ frontend/components/Pages/Records.jsx
@@ -1,11 +1,13 @@
 // React Components:
 import { useEffect, useState, useRef } from 'react';
 import { ListDetailLayout } from '../Layouts';
-import RecordItemView from '../RecordItemView';
 import DetailsView from '../DetailsView';
-import TopBar from '../TopBar';
-import RecordForm from '../RecordForm.jsx';
 import Form from '../Form.jsx';
+import ItemFilters from '../ItemFilters.jsx';
+import RecordItemView from '../RecordItemView';
+import RecordForm from '../RecordForm.jsx';
+import TopBar from '../TopBar';
+import HorizontalRow from '../HorizontalRow.jsx';
 
 // Material Components:
 import 'mdui/components/button-icon.js';
@@ -16,156 +18,76 @@ import 'mdui/components/menu-item.js';
 import 'mdui/components/text-field.js';
 
 // Local Imports:
-import { openDialog, closeDialog, searchFunction } from '../../assets/scripts.js'
-import HorizontalRow from '../HorizontalRow.jsx';
+import { openDialog, closeDialog } from '../../assets/scripts.js'
+import { useFetchData } from '../../assets/useFetchData.js';
+
 
 function Records() {
-    const [items, setItems] = useState([]);
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    // Global Properties
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    const {isLoading,items,reloadItems} = useFetchData("/api/records");
+    const [filteredItems, setFilteredItems] = useState(items);
     const snackBarRef = useRef(null);
-    const textSearchRef = useRef(null);
-    const loadingAnimationRef = useRef(null);
     const [selectedItem, setSelectedItem] = useState(null);
-    const [filteredItems, setFilteredItems] = useState([]);
-    const [searchQuery, setSearchQuery] = useState("");
 
-    useEffect(() => {
-        // Fetch Records:
-        fetchItems("/api/records");
-
-        // Initialize Search Function:
-        if(textSearchRef.current) textSearchRef.current.addEventListener("input", textSearch);
-
-        // Initialze ESC Key:
-        document.body.addEventListener("keydown", function(event) {
-            if (event.key === "Escape") {
-                closeDetails();
-            }
-        })
-
-    }, []); // fetch data only once
-
-    /**
-     * Update items if the filter changes
-     */
-    useEffect(() => {
-        let filtered = items
-        if(searchQuery) {
-            console.log("aplpy search query");
-            filtered = filtered.filter(item => searchFunction(item, searchQuery));
-            console.log("filtered.length = "+filtered.length);
-        }
-        setFilteredItems(filtered);
-    }, [items, searchQuery]);
-
-    /**
-     * Fetches items from the given path. The response has to be in JSON Lines format so they can
-     * be parsed dynamically
-     * @param {string} url path to fetch items from
-     */
-    async function fetchItems(url) {
-        // Enable Loading Animation:
-        loadingAnimationRef.current.loading = true;
-
-        try {
-            // Fetch Items:
-            const response = await fetch(url);
-            if(!response.ok) {
-                snackBarRef.current.textContent = `Failed to fetch records [${response.status} ${response.statusText}]`;
-                snackBarRef.current.open = true;
-                return;
-            }
-            const reader = response.body.getReader()// decode bytes to text chungs
-            setItems([]); // clear current items after successful request
-
-            // Read Incoming Stream:
-            let buffer = ''; // byte buffer to accumulate chunks
-            while(true) {
-                const { done, value } = await reader.read();
-                buffer += new TextDecoder().decode(value);
-                let newlineIndex;
-                while((newlineIndex = buffer.indexOf('\n')) !== -1) {
-                    const line = buffer.substring(0, newlineIndex);
-                    buffer = buffer.substring(newlineIndex + 1); // advance buffer past the line (including the newline character)
-                    let trimmed = line.trim() // remove whitespaces
-                    if(!trimmed) { continue; } // skip loop on empty line 
-                    try {
-                        const jsonObject = JSON.parse(trimmed);
-                        setItems(prevItems => [...prevItems, jsonObject])
-                    } catch(e) {
-                        console.error(`Error parsing JSON "${trimmed}": ${e}`);
-                    }
-                }
-                if(done) { break; }
-            }
-        } catch (error) {
-            console.error("Failed to fetch items:", error);
-        } finally {
-            // Disable Loading Animation:
-            loadingAnimationRef.current.loading = false;
-        }
-    }
-
-    function openDetails(id) {
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    // Event Handler (triggered on user interactions):
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    function showDetails(id) {
         setSelectedItem(items.find(item => item.id === id));
     };
 
-    function closeDetails() {
+    function hideDetails() {
         setSelectedItem(null);
     };
 
-    function textSearch() {
-        const query = textSearchRef.current.value;
-        setSearchQuery(String(query).toLocaleLowerCase());
-    }
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    // Hooks:
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    // Initialize:
+    useEffect(() => {
+        // Initialze ESC Key:
+        document.body.addEventListener("keydown", function(event) {
+            if (event.key === "Escape") {
+                hideDetails();
+            }
+        });
+    }, []);
 
+    // Update Loading Animation:
+    const loadingAnimationRef = useRef(null);
+    useEffect(() => {
+        loadingAnimationRef.current.loading = isLoading;
+    }, [isLoading]);
 
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    // Sub-Elements:
+    ///////////////////////////////////////////////////////////////////////////////////////////////
     const ListHeader = (
         <>
-            <HorizontalRow>
-                <mdui-tooltip content="Add new record">
-                    <mdui-fab ref={loadingAnimationRef} onClick={() => (openDialog("add-record"))} icon="note_add">Add new record</mdui-fab>
-                </mdui-tooltip>
-                <mdui-text-field ref={textSearchRef} type="search" label="Search for records or container names" clearable>
-                    <mdui-icon slot="icon" name="search"></mdui-icon>
-                </mdui-text-field>
-            </HorizontalRow>
-            <section style={{alignItems:"center", display:"flex", gap:"8px", overflowX:"auto", justifyContent:"flex-start"}}>
-            </section>
-            <span className="info-text">Showing {filteredItems.length} of {items.length}</span>
+            <mdui-collapse>
+                <mdui-collapse-item trigger="#showFilters">
+                    <div slot="header" style={{alignContent:"flex-end", alignItems:"center", display:"flex", gap:"12px", justifyContent:"space-between", padding:"12px 0.5rem 0.5rem"}}>
+                        <mdui-fab extended ref={loadingAnimationRef} onClick={() => {openDialog("add-record")}} icon="note_add">Add new record</mdui-fab>
+                        <mdui-button id="showFilters" variant="text" end-icon="keyboard_arrow_down">Use filters</mdui-button>
+                    </div>
+                    <div style={{padding:"0 0.5rem"}}>
+                        <ItemFilters items={items} updateFilteredItems={setFilteredItems} />
+                    </div>
+                </mdui-collapse-item>
+            </mdui-collapse>
             <mdui-dialog id="add-record" close-on-esc close-on-overlay-click>
                 <TopBar title="Add new record" closeFunction={() => (closeDialog("add-record"))}></TopBar>
-                <Form action="#">
-                    <section>
-                        <div>
-                            <mdui-text-field label="Last seen" type="datetime-local" name="time"></mdui-text-field>
-                        </div>
-                        <div>
-                            <mdui-select label="Select Category" value="Error" name="category">
-                                <mdui-menu-item value="Critical">Critical</mdui-menu-item>
-                                <mdui-menu-item value="Error">Error</mdui-menu-item>
-                                <mdui-menu-item value="Warning">Warning</mdui-menu-item>
-                                <mdui-menu-item value="Info">Info</mdui-menu-item>
-                                <mdui-menu-item value="Debug">Debug</mdui-menu-item>
-                            </mdui-select>
-                        </div>
-                        <div>
-                            <mdui-text-field label="Docker Container" value="" defaultValue="" name="source"></mdui-text-field>
-                        </div>
-                        <div>
-                            <mdui-text-field label="Log Message" value="" defaultValue="" name="message" autosize max-rows="7" enterkeyhint="enter" ></mdui-text-field>
-                        </div>
-                        <div>
-                            <mdui-text-field label="Add Solution" value="" defaultValue="" name="solution" autosize max-rows="7" enterkeyhint="enter"></mdui-text-field>
-                        </div>
-                    </section>
-                </Form>
+                <RecordForm action="#"></RecordForm>
             </mdui-dialog>
+            <span className="info-text">Showing {filteredItems.length} of {items.length}</span>
         </>
     );
 
     const ListPane = filteredItems.length > 0 ? (
         <mdui-list>
-            {filteredItems.map(item => (<RecordItemView key={item.id} log={item} onClick={openDetails} isSelected={item === selectedItem} />))}
+            {filteredItems.map(item => (<RecordItemView key={item.id} log={item} onClick={showDetails} isSelected={item === selectedItem} />))}
         </mdui-list>
     ) : (
         <div style={{alignItems:'center', display:'flex', justifyContent:'center', margin:'auto'}}>
@@ -174,9 +96,9 @@ function Records() {
         </div>
     );
 
-    const TopBarElement = selectedItem && (
+    const DetailsTopBarElement = selectedItem && (
         <>
-            <TopBar title={"#"+selectedItem.id} closeFunction={closeDetails}>
+            <TopBar title={"#"+selectedItem.id} closeFunction={hideDetails}>
                 <mdui-button-icon icon="delete" onClick={() => (openDialog("delete-record"))}></mdui-button-icon>
                 <mdui-button-icon icon="edit" onClick={() => (openDialog("edit-record"))}></mdui-button-icon>
             </TopBar>
@@ -188,7 +110,7 @@ function Records() {
                 <TopBar title={"Delete Record #"+selectedItem.id} closeFunction={() => (closeDialog("delete-record"))}></TopBar>
                 <Form action="#">
                     <section>
-                        <input type="hidden" name="confirm" value={true}/>
+                        <input type="hidden" name="id" value={selectedItem.id}/>
                         Do you want to delete this record?
                     </section>
                 </Form>
@@ -197,7 +119,7 @@ function Records() {
     );
 
     const DetailPane = selectedItem && (
-        <DetailsView top={TopBarElement} log={selectedItem} />
+        <DetailsView top={DetailsTopBarElement} log={selectedItem} />
     );
 
     return(
