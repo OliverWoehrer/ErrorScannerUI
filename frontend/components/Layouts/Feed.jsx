b45bd11895d5db5566f99b976a67ae251d273b14
OliverWoehrer <oliver.woehrer@gmail.com>
2025-11-12 14:47:33 +0100

    add bottom spacer for smaller screens to compensate for navigation bottom bar

--- frontend/components/Layouts/Feed.jsx
+++ frontend/components/Layouts/Feed.jsx
@@ -4,12 +4,18 @@
 
 // Local Import:
 import "./style.css"
-
+import useScreenSize from '../../hooks/useScreenSize.js';
+import { useEffect } from "react";
 
 function Feed({ children }) {
+    // Layout Conditionals
+    const { isAtMost } = useScreenSize();
+    const isSmallScreen = isAtMost('small'); // bottom navigation for small screens (< 601px)
+
     return (
         <div className="feed-layout">
             {children}
+            {isSmallScreen && (<div style={{height:"70px"}}></div>)}
         </div>
     );
 }
