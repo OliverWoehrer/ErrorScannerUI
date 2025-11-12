737ce09fb7767034b37574c35a269a7cfb827de1
OliverWoehrer <oliver.woehrer@gmail.com>
2025-11-12 17:05:38 +0100

    submit from data without page reload

--- frontend/components/Form.jsx
+++ frontend/components/Form.jsx
@@ -6,23 +6,46 @@ function defaultSubmit(event) {
     event.preventDefault(); // prevent automatic form submisson
 };
 
-function manualSubmit(event) {
-    const form = document.getElementById("my-form");
-    snackbar({ message: "Form submitted", closeable:true });
-    // TODO: send post request manually
-    // form.submit() will trigger a page reload
+async function sendData(event) {
+    // Build JSON Form Data:
+    const form = event.target.closest("form");
+    if(!form) {
+        snackbar({ message:`Could not find parent <form> element`, closeable:true });
+        return;
+    }
+    const formData = new FormData(form);
+    const formDataObject = Object.fromEntries(formData.entries());
+    const formDataJsonString = JSON.stringify(formDataObject); // convert to serialized JSON string
+    const endpoint = form.getAttribute('action');
+
+    // Send JSON Data:
+    try {
+        const response = await fetch(endpoint, {
+            method: "POST",
+            headers: { "Content-Type":"application/json" },
+            body: formDataJsonString,
+        });
+        if(response.ok) {
+            snackbar({ message:`Submitted data successfully`, closeable:true });
+        } else {
+            snackbar({ message:`Failed to submit data to '${endpoint}' [${response.status} ${response.statusText}]`, closeable:true });
+            return;
+        }
+    } catch(error) {
+        snackbar({ message:`Failed to submit data to '${endpoint}' [${error}]`, closeable:true });
+    }
 }
 
 function Form({ action, children }) {
     return(
-        <form id="my-form" action={action} method="GET" onSubmit={defaultSubmit} style={{alignItems:"stretch", display:"flex", flexDirection:"column", height:"100%", justifyContent:"space-between"}}>
+        <form name="my-form" action={action} method="GET" onSubmit={defaultSubmit} style={{alignItems:"stretch", display:"flex", flexDirection:"column", height:"100%", justifyContent:"space-between"}}>
             {/* TODO: change method to POST */}
             <div>
                 {children}
             </div>
             <div style={{alignSelf:"flex-end", float:"right", marginTop:"12px"}}>
                 <mdui-button type="reset" variant="text">Discard changes</mdui-button>
-                <mdui-button type="submit" onClick={manualSubmit}>Confirm</mdui-button>
+                <mdui-button type="submit" onClick={sendData}>Confirm</mdui-button>
             </div>
         </form>
     );
