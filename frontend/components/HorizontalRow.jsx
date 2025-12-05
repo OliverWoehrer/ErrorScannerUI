6f176d98eae9cc7c40757bdbf22da3000a3b69eb
OliverWoehrer <oliver.woehrer@gmail.com>
2025-12-05 15:02:37 +0100

    added flex styles to style sheet

--- frontend/components/HorizontalRow.jsx
+++
@@ -1,9 +0,0 @@
-function HorizontalRow({ overflow, children }) {
-    return (
-        <div style={{alignContent:"flex-end", alignItems:"center", display:"flex", gap:"12px", justifyContent:"flex-start", overflowX:(overflow ? "auto" : "")}}>
-            {children}
-        </div>
-    );
-}
-
-export default HorizontalRow;
