da314f4852a4656fc29bc0902b4ddd21749ef128
OliverWoehrer <oliver.woehrer@gmail.com>
2025-12-11 18:43:18 +0100

    removed legacy implementation

--- backend/data/__init__.py
+++ backend/data/__init__.py
@@ -2,13 +2,11 @@
 This module implements functions to read and write data files in here
 """
 from .data_item import DataItem
-
 from .config import ConfigHandler
-from .logsrecords import LogsRecordsHandler
 from .logs import LogsFile
 from .records import RecordsFile
 
 config_data = ConfigHandler("config.json")
 logs_data = LogsFile("logs.jsonl")
-records_data = RecordsFile("records.json")
+records_data = RecordsFile("records.jsonl")
 records_data.load()
